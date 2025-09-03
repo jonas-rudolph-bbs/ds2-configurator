4a0e15ff48be9585761c1e7137d4f2969ea87157
jrud <jonas.rudolph@softwareag.com>
2025-09-03 13:33:49 +0200

    initial commit

---
+++ src/app/routes.ts
@@ -0,0 +1,18 @@
+import {provideRouter} from '@angular/router';
+import {Routes} from '@angular/router';
+import {Home} from './home/home';
+import {Details} from './details/details';
+
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: Details,
+    title: 'Home details',
+  },
+];
+export default routeConfig;
